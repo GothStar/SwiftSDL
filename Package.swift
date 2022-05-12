6c83835137042877335ab45c03db57e5fc230ce6
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-05-12 16:50:23 +0200

    Adding SDL_Image support

--- Package.swift
+++ Package.swift
@@ -8,6 +8,9 @@ let package = Package(
         .library(
             name: "SDL",
             targets: ["SDL"]),
+        .library(
+            name: "SDLImage",
+            targets: ["SDLImage"]),
         .executable(
             name: "SDLDemo",
             targets: ["SDLDemo"]),
@@ -19,12 +22,22 @@ let package = Package(
         .target(
             name: "SDL",
             dependencies: ["CSDL2"]),
+        .target(
+            name: "SDLImage",
+            dependencies: ["CSDL2_Image"]),
         .systemLibrary(
             name: "CSDL2",
             pkgConfig: "sdl2",
             providers: [
                 .brew(["sdl2"]),
                 .apt(["libsdl2-dev"])
+            ]),
+        .systemLibrary(
+            name: "CSDL2_Image",
+            pkgConfig: "sdl2_image",
+            providers: [
+                .brew(["sdl2_image"]),
+                .apt(["libsdl2-image-dev"])
             ]),
         .testTarget(
             name: "SDLTests",
---
+++ Sources/SDLImage/SDLImage.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Francisco Javier Trujillo Mata on 12/5/22.
+//
+
+import Foundation
